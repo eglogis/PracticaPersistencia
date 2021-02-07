aa3d886aab1e8b6e3fb178d479ff565215686847
Samuel <samu6333@gmail.com>
2021-02-07 20:16:39 +0100

    Add initial notebook view controller

--- ProyectoPersistencia/App/SceneDelegate.swift
+++ ProyectoPersistencia/App/SceneDelegate.swift
@@ -16,7 +16,17 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+
+        guard let notebooksViewController = UIStoryboard(
+                name: "Notebooks",
+                bundle: nil
+        ).instantiateViewController(identifier: "NotebooksViewController") as? NotebooksViewController else {
+            fatalError("NotebookTableViewController could not be created.")
+        }
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        self.window = UIWindow(windowScene: windowScene)
+        self.window?.rootViewController = UINavigationController(rootViewController: notebooksViewController)
+        self.window?.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
---
+++ ProyectoPersistencia/Screens/Notebooks/NotebooksViewController.swift
@@ -0,0 +1,15 @@
+//
+//  NotebooksViewController.swift
+//  ProyectoPersistencia
+//
+//  Created by Samuel Bautista Sánchez on 7/2/21.
+//
+
+import UIKit
+
+class NotebooksViewController: UIViewController {
+
+    override func viewDidLoad() {
+        
+    }
+}
